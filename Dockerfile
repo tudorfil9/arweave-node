db65b2947dec459d64f55b64ab0277ce486e2f2a
Tudor Fil <tudor.fil@netcentric.biz>
2020-09-10 14:27:13 +0300

    docker container bind type volume support for persistent weave data

--- Dockerfile
+++ Dockerfile
@@ -1,3 +1,5 @@
+ ### ARWEAVE ####
+ # file: Dockerfile
 # Use the official image as a parent image.
 FROM ubuntu:latest
 # Update and prep
@@ -11,7 +13,7 @@ RUN wget https://github.com/ArweaveTeam/arweave/releases/download/N.2.1.0.2/arwe
 
 WORKDIR /appl/arweave/bin/
 
-RUN git clone https://github.com/tudorfil9/arweave-node.git /appl/arweave-node && cp /appl/arweave-node/start_w_epmd.sh .
+RUN git clone -b develop https://github.com/tudorfil9/arweave-node.git /appl/arweave-node && cp /appl/arweave-node/start_w_epmd.sh .
 
 ENTRYPOINT chmod +x ./start_w_epmd.sh && ./start_w_epmd.sh 
 
--- start_w_epmd.sh
+++ start_w_epmd.sh
@@ -1,18 +1,30 @@
+  ### ARWEAVE ####
+ # file: start_w_epmd.sh
  #!/bin/bash
 
-../erts-10.3.5/bin/epmd -daemon && \
-/bin/bash ./start data_dir /mnt/arweave-data \
-                    max_miners 0 \
-                    peer 159.203.158.108 \
-                    peer 138.197.232.192 \
-                    peer 134.209.27.233 \
-                    peer 167.71.128.173 \
-                    peer 134.209.27.239 \
-                    peer 209.97.191.10 \
-                    peer 46.101.67.172 \
-                    requests_per_minute_limit 3000 \
-                    disk_space 1000000 \
-                    tx_propagation_parallelization 3 \
-                    max_emitters 1 \
-                    enable arql_tags_index \
-                    port 1984
+
+while getopts runmode: flag
+	do
+	    case "${flag}" in
+	        runmode) runmode=${OPTARG};;
+	    esac
+	done
+	echo "runmode: $runmode";
+
+
+# ../erts-10.3.5/bin/epmd -daemon && \
+# /bin/bash ./start data_dir /mnt/arweave-data \
+#                     max_miners 0 \
+#                     peer 159.203.158.108 \
+#                     peer 138.197.232.192 \
+#                     peer 134.209.27.233 \
+#                     peer 167.71.128.173 \
+#                     peer 134.209.27.239 \
+#                     peer 209.97.191.10 \
+#                     peer 46.101.67.172 \
+#                     requests_per_minute_limit 3000 \
+#                     disk_space 1000000 \
+#                     tx_propagation_parallelization 3 \
+#                     max_emitters 1 \
+#                     enable arql_tags_index \
+#                     port 1984
